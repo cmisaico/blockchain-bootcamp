eedea9218d54109b4fb97236cb9bbafaa3750824
Christian Misaico <christianmisaico@gmail.com>
2023-12-18 15:49:20 -0500

    feat: Example ERC1155

--- 04_token_erc1155/customERC1155.sol
+++ 04_token_erc1155/customERC1155.sol
@@ -6,5 +6,22 @@ contract erc1155 is ERC1155 {
 
     // Variables
 
+    uint256 public constant GOLD = 0;
+    uint256 public constant SILVER = 1;
+    uint256 public constant THORS_HAMMER = 2;
+    uint256 public constant SWORD = 3;
+    uint256 public constant SHIELD = 4;
+
+    // Constructor 
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+        _mint(msg.sender, GOLD, 10**18, "");
+        _mint(msg.sender, SILVER, 10**27, "");
+        _mint(msg.sender, THORS_HAMMER, 1, "");
+        _mint(msg.sender, SWORD, 10**9, "");
+        _mint(msg.sender, SHIELD, 10**9, "");
+
+        _mint(msg.sender, [GOLD, SILVER], [10**18, 10**27], "");
+    }
+
 
 }
